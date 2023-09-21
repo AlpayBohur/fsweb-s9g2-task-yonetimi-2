fec817651d88cd25a55223014afe64d398bb18b9
Timuçin Alpay Bohur <bohuralpay@gmail.com>
2023-09-21 05:19:19 +0300

    teslim

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      color: {
+        pill: "#ccc",
+        task: "#c8781a",
+      },
+    },
+  },
+  plugins: [],
+};
